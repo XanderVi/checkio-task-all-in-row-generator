c4284a5a1f983f6d6f6e4351344634c211c355ba
oduvan <a.lyabah@checkio.org>
2017-03-13 23:44:58 +0200

    js and initial code

--- verification/referee.py
+++ verification/referee.py
@@ -5,16 +5,12 @@ from checkio.referees.io import CheckiOReferee
 from tests import TESTS
 
 
-def forbidden(code, runner):
-    if len(code) >= 140:
-        return False, "Hm, I counted {} symbols.".format(len(code))
-    return True, "ok"
-
-
 api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
-        function_name="flat_list",
-        inspector=forbidden
+        function_name={
+            "python": "flat_list",
+            "js": "flatList"
+        }
     ).on_ready)
