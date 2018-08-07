476467d47c3b507e11b5b49843412e75851fb1dd
XanderVi <engineermexanic@gmail.com>
2018-08-07 18:23:28 +0300

    add to github

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -2,7 +2,6 @@ requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
     function (extIO, $, TableComponent) {
         var io = new extIO({
             functions: {
-                js: 'flatList',
                 python: 'flat_list'
             }
         });
